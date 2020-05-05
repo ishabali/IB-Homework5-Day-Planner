8432952de6a7a204d62b2b14982feaedc283bdc8
Isha Bali <ishabali@gmail.com>
2020-05-04 20:41:31 -0500

    Current Data and Time Displayed

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
+    var x = "variable";
+    // $('#currentDay').html('<h1>a '+ x + ' b</h1>');
+    // is same as
+    //$('#currentDay').html(`<h1>a  ${x}  b</h1>`);
 
+    $('#currentDay').html(`<h1>${moment().format('llll')}</h1>`);
+   
    
     // $('#highScores').on('click', function(){
   
